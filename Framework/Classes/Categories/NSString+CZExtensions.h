e3ac659c1de46e4beb8b8b1f78284722d59858f0
Carter Allen <CarterA@opt-6.com>
2010-01-28 20:48:56 -0700

    Added documentation to the NSString category.

--- Framework/Classes/Categories/NSString+CZExtensions.h
+++ Framework/Classes/Categories/NSString+CZExtensions.h
@@ -5,15 +5,42 @@
 //  Created by Carter Allen on 1/14/10.
 //  Copyright 2010 Opt-6 Products, LLC. All rights reserved.
 //
+/** Adds methods for checking if a string is empty, as well as removing empty characters and basic RegEx support. */
 
 #import <Foundation/Foundation.h>
 
 @interface NSString (CZExtensions)
 #pragma mark Emptiness Checking
+/** Checks whether the NSString object is empty.￼
+
+This is a convenience method which runs isEmptyIgnoringWhitespace:YES.
+
+@return The result of isEmptyIgnoringWhitespace:YES.
+@see isEmptyIgnoringWhitespace:
+*/
 - (BOOL)isEmpty;
+/** Checks whether the NSString￼ object is empty, optionally ignoring whitespace.
+
+@param ignoresWhitespace Whether or not to count whitespace as empty or not. YES will count whitespace as empty,
+ NO will not.
+@return YES if the string is empty, NO if the string contains characters.
+*/
 - (BOOL)isEmptyIgnoringWhitespace:(BOOL)ignoresWhitespace;
 #pragma mark Whitespace Removal
+/** Create a new NSString object by removing whitespace from the beginning and end of the original NSString.￼
+
+This will remove characters in the whitespaceCharacterSet, as defined by NSCharacterSet.
+
+@return An initialized string based on the original with whitespace trimmed off.
+*/
 - (NSString *)stringByTrimmingWhitespace;
 #pragma mark RegEx Support
+/** Uses NSPredicate to check if the NSString matches the provided regular expression.￼
+
+This is only for very simple RegEx needs, as it simply wraps NSPredicate.
+
+@param expression A regular expression.
+@return YES if the NSString matches the expression, and NO if it does not.
+*/
 - (BOOL)matchesRegularExpression:(NSString *)expression;
 @end
