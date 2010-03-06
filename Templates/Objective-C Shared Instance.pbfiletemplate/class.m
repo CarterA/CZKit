f67f95b06231ab7c6fe674214663c37550b7c03d
Carter Allen <CarterA@opt-6.com>
2010-03-05 21:07:10 -0700

    FIxed Shared Instance template so that it actually, you know, works.

--- Templates/Objective-C Shared Instance.pbfiletemplate/class.m
+++ Templates/Objective-C Shared Instance.pbfiletemplate/class.m
@@ -7,24 +7,15 @@
 //
 
 «OPTIONALHEADERIMPORTLINE»
-static «FILEBASENAMEASIDENTIFIER» global«FILEBASENAMEASIDENTIFIER»;
+static «FILEBASENAMEASIDENTIFIER» *global«FILEBASENAMEASIDENTIFIER»;
 @implementation «FILEBASENAMEASIDENTIFIER»
-+ («FILEBASENAMEASIDENTIFIER»)shared«FILEBASENAMEASIDENTIFIER» {
++ («FILEBASENAMEASIDENTIFIER» *)shared«FILEBASENAMEASIDENTIFIER» {
 	@synchronized(self) {
 		if (!global«FILEBASENAMEASIDENTIFIER») global«FILEBASENAMEASIDENTIFIER» = [[self alloc] init];
 		return global«FILEBASENAMEASIDENTIFIER»;
 	}
 }
-- (id)allocWithZone:(NSZone *)zone {
-	@synchronized(self) {
-		if (self == nil) {
-			global«FILEBASENAMEASIDENTIFIER» = [super allocWithZone:zone];
-			return global«FILEBASENAMEASIDENTIFIER»;
-		}
-	}
-	return nil;
-}
-- (id)copyWithZone:(NSZone *)zone { return self; }
++ (id)copyWithZone:(NSZone *)zone { return self; }
 - (id)retain { return self; }
 - (NSUInteger)retainCount { return NSUIntegerMax; }
 - (void)release {}
